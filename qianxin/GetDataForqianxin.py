4ff3e50da0a86a77d463548efeba1fdc16e62eb4
H-Limbus <2275908981@qq.com>
2023-03-11 10:09:58 +0800

    qianxin API

--- qianxin/GetDataForqianxin.py
+++ qianxin/GetDataForqianxin.py
@@ -3,24 +3,31 @@
 # @Author  :  Limbus
 #  @file   :  GetDataForqianxin.py
 
+
+"""
+qianxin(鹰图平台) use api to get data
+"""
+
 import time
-import requests
-from config.Config import (QIANXIN_API_KEY, DEFAULT_START_TIME, DEFAULT_END_TIME, ageHalfYearDate, ageOneMonthDate)
 import base64
+import requests
 from math import ceil
 from alive_progress import alive_bar
+from config.Config import (QIANXIN_API_KEY, DEFAULT_START_TIME, DEFAULT_END_TIME, ageHalfYearDate, ageOneMonthDate)
 
 
 def GDFqianxin(logger):
     session = requests.Session()
     searchDate = ''
-    searchSyntax = base64.urlsafe_b64encode(input('请输入qianxin 查询语法：').encode("utf-8")).decode('utf-8')
+    searchSyntax = base64.urlsafe_b64encode(input('input qianxin search syntax: ').encode("utf-8")).decode('utf-8')
     print('''
-        按照序号选择查询数据的时间节点（默认一年以内）
-            1、一个月以内
-            2、半年以内
+        select time node (default is within one year)
+
+            1、one month
+            2、half year
+
     ''')
-    s = input('请输入选择的序号（要是选择默认，直接回车）：')
+    s = input('input the time you choose(default Enter): ')
     if s == '1': searchDate = ageOneMonthDate
     elif s == '2': searchDate = ageHalfYearDate
     else: searchDate = DEFAULT_START_TIME
@@ -32,9 +39,9 @@ def GDFqianxin(logger):
     else:
         totalCount = page['data']['total']
         restQuota = page['data']['rest_quota'].replace('今日剩余积分：', '')
-        logger.info(f'您查询的语法共有{totalCount}条，今日剩余积分有{restQuota}分。')
+        logger.info(f'There are {totalCount}t data，rest quota: {restQuota}.')
         while 1:
-            getDataCount = input(f'查询1条数据1积分，获得全部资源需要{totalCount}分，您需要多少条数据，(退出输入q)：')
+            getDataCount = input(f'1 data = 1 quota, how much data you need (q exit):')
             if getDataCount == 'q': exit()
             if int(getDataCount) <= 10:
                 data = page['data']['arr']
@@ -42,7 +49,7 @@ def GDFqianxin(logger):
                 for i in data:
                     sumData.append(i['ip'] + ':' + str(i['port']))
                 yield sumData
-                logger.info('今日剩余积分： ' + page['data']['rest_quota'])
+                logger.info('remaining quota today:  ' + page['data']['rest_quota'])
                 break
             elif int(getDataCount) <= int(restQuota):
                 data = page['data']['arr']
@@ -51,7 +58,7 @@ def GDFqianxin(logger):
                     sumData.append(i['ip'] + ':' + str(i['port']))
                 yield sumData
                 queryLinkSum = [url.format(QIANXIN_API_KEY, searchSyntax, i, searchDate, DEFAULT_END_TIME) for i in range(2, ceil(int(getDataCount)/10)+1)]
-                with alive_bar(len(queryLinkSum), unknown="stars", title="获取中") as bar:
+                with alive_bar(len(queryLinkSum), unknown="stars", title="getting") as bar:
                     for Link in queryLinkSum:
                         sumData = []
                         html = session.get(Link).json()
@@ -60,13 +67,13 @@ def GDFqianxin(logger):
                             for i in html['data']['arr']:
                                 sumData.append(i['ip'] + ':' + str(i['port']))
                         except:
-                            logger.info(html['message'])
+                            logger.error(html['message'])
                         yield sumData
                         bar()
                 break
             elif int(getDataCount) > int(restQuota):
-                logger.error(f'您今日积分不够查询{getDataCount}条数据。请重新输入!')
+                logger.error('the remaining points today are not enough to query{getDataCount}, try again! ')
             else:
-                logger.error('输入有误，请重新输入!')
+                logger.error('input error, try again! ')
 
 
